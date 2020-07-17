73fee108278e94345f9861c9bec86afe54d38669
sraj-dev-hub <sraj.devproducts@gmail.com>
2020-07-18 00:24:47 +0530

    add build file

---
+++ Frontend/build/precache-manifest.56b2a3dbb1332be012d89ea46fb8ed3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c66857877bd1018a38102c1cf1fae74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "139bdc63c985fcc24f4e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c89f5e1af0ef5e08e176",
+    "url": "/static/js/2.4280a9c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4280a9c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139bdc63c985fcc24f4e",
+    "url": "/static/js/main.a0c58f98.chunk.js"
+  },
+  {
+    "revision": "63ba08987322a76962dc",
+    "url": "/static/js/runtime-main.ec26715b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
