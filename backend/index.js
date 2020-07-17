397548c8b62a5b3da84719b7ad7392a6f9f15f2e
sraj-dev-hub <sraj.devproducts@gmail.com>
2020-07-17 23:54:10 +0530

    initial code

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3002;
+var path = require("path");
+
+app.use(
+  "/static",
+  express.static(path.join(__dirname, "../Frontend/build/static"))
+);
+app.get("/", (req, res) => {
+  console.log(path.resolve(__dirname + "../Frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "../Frontend/build/index.html"));
+  //   res.send("This is from express.js");
+});
+
+app.listen(PORT, () => {
+  console.log("server started on port 3002");
+});
