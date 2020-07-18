e7a4339c824904cda1e200d558923bcc1795cf0b
sraj-dev-hub <sraj.devproducts@gmail.com>
2020-07-18 15:20:47 +0530

    code changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,8 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo}  alt="logo" />
-       
+        <img src={logo} className="banner" alt="logo" />    
         
       </header>
     </div>
