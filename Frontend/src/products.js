f1574919726635d1f4783d284ee1ef451bebefdb
sraj-dev-hub <sraj.devproducts@gmail.com>
2020-08-02 18:58:00 +0530

    code changes

--- Frontend/src/products.js
+++ Frontend/src/products.js
@@ -47,6 +47,181 @@ export const products = [
     desc:
       "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
   },
+  
+  {
+    _id: "5",
+    name: "Chaos Yellow Mask",
+    image: "/images/p5.jpeg",
+    imageSet: ["/images/p5.jpeg", "/images/p5-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+
+  {
+    _id: "6",
+    name: "Chaos Yellow Mask",
+    image: "/images/p6.jpeg",
+    imageSet: ["/images/p6.jpeg", "/images/p6-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+
+
+  {
+    _id: "7",
+    name: "Chaos Yellow Mask",
+    image: "/images/p7.jpeg",
+    imageSet: ["/images/p7.jpeg", "/images/p7-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+
+
+
+  {
+    _id: "8",
+    name: "Chaos Yellow Mask",
+    image: "/images/p8.jpeg",
+    imageSet: ["/images/p8.jpeg", "/images/p8-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+
+  {
+    _id: "9",
+    name: "Chaos Yellow Mask",
+    image: "/images/p9.jpeg",
+    imageSet: ["/images/p9.jpeg", "/images/p9-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+
+
+
+  {
+    _id: "10",
+    name: "Chaos Yellow Mask",
+    image: "/images/p10.jpeg",
+    imageSet: ["/images/p10.jpeg", "/images/p10-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+
+
+  {
+    _id: "11",
+    name: "Chaos Yellow Mask",
+    image: "/images/p11.jpeg",
+    imageSet: ["/images/p11.jpeg", "/images/p11-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+
+  {
+    _id: "12",
+    name: "Chaos Yellow Mask",
+    image: "/images/p12.jpeg",
+    imageSet: ["/images/p12.jpeg", "/images/p12-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+  {
+    _id: "13",
+    name: "Chaos Yellow Mask",
+    image: "/images/p13-2.jpeg",
+    imageSet: ["/images/p13-2.jpeg", "/images/p13-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+  {
+    _id: "14",
+    name: "Chaos Yellow Mask",
+    image: "/images/p14.jpeg",
+    imageSet: ["/images/p14.jpeg", "/images/p14-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+  {
+    _id: "15",
+    name: "Chaos Yellow Mask",
+    image: "/images/p15.jpeg",
+    imageSet: ["/images/p15.jpeg", "/images/p15-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+  {
+    _id: "16",
+    name: "Chaos Yellow Mask",
+    image: "/images/p16.jpeg",
+    imageSet: ["/images/p16.jpeg", "/images/p16-2.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+  {
+    _id: "17",
+    name: "Chaos Yellow Mask",
+    image: "/images/p17.jpeg",
+    imageSet: ["/images/p17.jpeg", "/images/p17.jpeg"],
+    price: 40,
+    inStock: true,
+    shortDesc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton.",
+    desc:
+      "Non Surgical Face Mask. breathable and reusable mask made with fine weaved cotton with 2 layers. Wider face coverage for maximum protection   ",
+  },
+
+
+
+
+
 ];
 
 export const getById = (id) => {
